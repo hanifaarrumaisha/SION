86f7f352ba387080306d7a8175251a20a88218b5
Hanifa Arrumaisha <hanifa.aisha@gmail.com>
2018-05-20 06:48:55 +0700

    add models.py login

--- SION/settings.py
+++ SION/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hanifaarrumaisha',                      
         'USER': 'postgres',
-        'PASSWORD': '',
+        'PASSWORD': 'calonskom2020',
         'HOST': 'localhost',
         'PORT': '5432',
     }
